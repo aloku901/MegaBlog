ffd6ca8441d9dbe4834fbf40b2555b1af07ff795
aloku901 <aloku901@gmail.com>
2023-09-27 22:37:46 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.REACT_APP_APPWRITE_URL);
+
+
+  return (
+    <>
+      <h1>A blog web with ALOK</h1>
+    </>
+  )
+}
+
+export default App
