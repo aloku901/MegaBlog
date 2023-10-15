ab6fe8098849f17911527a49c486b85942c6e524
Alok Upadhyay <aloku901@gmail.com>
2023-10-15 23:11:34 +0530

    Add some update

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -14,13 +14,26 @@ function signup() {
   const create = async(data) => {
     setError("");
     try {
-      
+      const userData = await authService.createAccount(data);
+      if(userData) {
+        const userData = await authService.getCurrentUser();
+        if(userData) dispatch(login(userData));
+        navigate("/")
+      }
     } catch (error) {
       setError(error.messaget);
     }
   }
   return (
-    <div>signup</div>
+    <div className='flex items-center justify-center'>
+      <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+        <div className='mb-2 flex justify-center'>
+          <span className='inline-block w-full max-w-[100px]'>
+            <Logo width='100%'/> 
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
