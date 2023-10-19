0cc3c16e7b695e2c67611e9d2a9a04ebbab1bd31
Alok Upadhyay <aloku901@gmail.com>
2023-10-19 23:09:43 +0530

    Add Login & Signup complete setup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -32,6 +32,42 @@ function signup() {
             <Logo width='100%'/> 
           </span>
         </div>
+        <h2 className='text-center text-2xl font-bold leading-tight'>Sign in to your account</h2>
+            <p className='mt-2 text-center text-base text-black/60'>
+                Already have an account?&nbsp;
+                <Link className='font-medium text-primary transition-all duration-200 hover:underline'>
+                    Sign In
+                </Link>
+            </p>
+            {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
+            <form onSubmit={handleSubmit(create)}>
+              <div className='space-y-5'>
+                <Input
+                  label="Full Name: " placeholder="Enter your full Name"
+                  {...register("name", {required: true,})}
+                />
+                <Input
+                    label="Email: "
+                    placeholder="Enter Your email"
+                    type="email"
+                    {...register("email"), {
+                        required: true,
+                        validate: {
+                            matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || "Email address must be a valid address",
+                        }
+                    }}
+                />
+                <Input
+                label="password: "
+                type="password"
+                placeholder="Enter your password"
+                {...register("password", {
+                  required: true,
+                })}
+                />
+                <Button type="submit" className="w-full">Create Account</Button>
+              </div>
+            </form>
       </div>
     </div>
   )
